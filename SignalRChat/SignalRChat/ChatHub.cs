51bbf8951437e82629e23de03c152adb65753446
Qingzhi <spiderman0828@gmail.com>
2016-04-06 11:10:34 -0400

    Added files via upload

---
+++ SignalRChat/SignalRChat/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChat
+{
+    using Microsoft.AspNet.SignalR;
+
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            var time = DateTime.Now.ToString();
+            Clients.All.addNewMessageToPage(name, message, time);
+        }
+
+    }
+}
---
+++ SignalRChat/SignalRChat/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
